8062e12e8bfcf27bcd7960ad48bc5a2d79aee129
Grzegorz Łuszczek <grzegorz@piklus.pl>
2014-06-14 12:41:11 +0200

    Fix render method in helper

--- lib/videojs_rails/view_helpers.rb
+++ lib/videojs_rails/view_helpers.rb
@@ -2,8 +2,7 @@ module VideojsRails
   module ViewHelpers
     def videojs_rails(*options, &blk)
       @options = options.extract_options!
-      render "videojs_rails/videojs_rails", &blk
+      render layout: "videojs_rails/videojs_rails", &blk
     end
   end
 end
-
