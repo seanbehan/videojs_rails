9b7ff63407c5bc2fa148e0d050cceff6a00933b0
Grzegorz Łuszczek <grzegorz@piklus.pl>
2014-06-14 12:59:20 +0200

    Add more configureable attributes

--- lib/videojs_rails/view_helpers.rb
+++ lib/videojs_rails/view_helpers.rb
@@ -1,7 +1,12 @@
 module VideojsRails
   module ViewHelpers
     def videojs_rails(*options)
-      @options = options.extract_options!
+      default_options = {
+        controls: true,
+        setup: "{}",
+        preload: "auto"
+      }
+      @options = default_options.merge(options.extract_options!)
       render "videojs_rails/videojs_rails"
     end
   end
