52ae104622569c969ee080a97fff58e6499f588f
Johannes Leers <johannes@leers.eu>
2015-09-08 10:28:55 +0200

    extend the detection of sources so you can drop in sth like {mp4_720: [type: 'video/mp4', src: '720.mp4', :'data-res' => '720p'],mp4_480: [type: 'video/mp4', src: '480.mp4', :'data-res' => '480p'] }. The traditional way works too: {mp4: 'file.mp4', webm: 'file.webm'}

--- lib/videojs_rails/tags/source.rb
+++ lib/videojs_rails/tags/source.rb
@@ -2,36 +2,27 @@ module VideojsRails
   module Tags
     class Source < Tag
       def initialize(video_type, options)
-        @video_type = video_type
-        parse_options(options)
+        @attributes = prepare_attributes(video_type, options)
       end
 
       def to_html
-        tag :source, attributes
+        tag :source, @attributes
       end
 
       private
 
-      attr_reader :video_type, :src
-
-      def parse_options(options)
+      def prepare_attributes(video_type, options)
         case options
-        when String, Symbol
-          @src = options
-        else
-          raise ArgumentError
+          when String, Symbol
+            {}.tap do |attributes|
+              attributes[:src] = options
+              attributes[:type] = "video/#{video_type}"
+            end
+          when Hash
+            options
+          else
+            raise ArgumentError
         end
-      end
-
-      def type
-        "video/#{video_type}"
-      end
-
-      def attributes
-        {
-          src: src,
-          type: type
-        }
       end
     end
   end
